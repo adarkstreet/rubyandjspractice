b2d7ae1459f004ea22f9de024656c438ab19df67
Andrew Street <andrew_street@live.com>
2018-02-21 17:04:08 -0700

    first commit

---
+++ js_practice.js
@@ -0,0 +1,21 @@
+// Write a function that accepts two integers and displays the larger.
+
+// function large(num1, num2) {
+//     if (num1 < num2( {
+//         return: num2,
+//       else 
+//         return: num1,
+//     })
+// };
+
+// Write a script which concatenates a given string 'n' times (default is 1).
+
+// function repeats(s, n) {
+//   var a = [];
+//   while (a.length < n) {
+//     a.push(s);
+//   };
+//   return a.join("");
+// };
+
+
---
+++ ruby_practice.rb
@@ -0,0 +1,25 @@
+# Write a script function that returns a passed string with letters in alphabetical order.
+# def alpha(string)
+#   puts "Sorted alpha is #{string.chars.sortj.join}"
+# end
+
+# Write a script that accepts a string as a parameter and find the longest word within the string.
+# def longest(string)
+#   string.max_by{ |x| x.length}
+# end
+
+# Write a script to apply a Bubble Sort algorithm.
+# def bs(a)
+#   return a if a.length <= (1)
+
+#   0.upto(a.length - 1) do |X|
+#     0 .upto(a.length - 2 -x) do |z|
+#     if a[i] = a[i + 1]
+#       a[i + 1], a[i] = a[i], a[i + 1]
+#      end
+#     end
+#   end
+#  bs(a) 
+# end
+
+# 
